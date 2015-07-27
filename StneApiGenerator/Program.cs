b0ffdab8a30ebdfce11d941f003060f05fe5e8e4
TheJP <jp_55555@hotmail.com>
2015-07-27 19:00:30 +0200

    Started to implement stne script translator

---
+++ StneApiGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace StneApiGenerator
+{
+    class Program
+    {
+        const string Url = "http://game3.stne.net/objectexplorer";
+        const string LinkPrefix = "ObjectExplorer.aspx?p=";
+
+        static void Main(string[] args)
+        {
+            //Download main list of elements (classes, enums, structs, ...)
+            var data = new WebClient().DownloadString(Url);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(data);
+
+            //Convert the html list to an actual list
+            var types = doc.DocumentNode.Descendants("a")
+                .Select(node => node.Attributes.First(attribute => attribute.Name == "href").Value)
+                .Where(link => link.StartsWith(LinkPrefix) && link.Length > LinkPrefix.Length)
+                .Select(link => link.Substring(LinkPrefix.Length));
+        }
+    }
+}
